90e3dc0381ea5bca2a0b01b0d516865b7de4682f
AgrebiOmar123 <oagrebi614@gmail.com>
2023-10-08 10:50:21 +0200

    first

---
+++ create.php
@@ -0,0 +1,28 @@
+<html>
+<body>    
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "stock";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO article (name, quantity, price)
+VALUES ($_POST['name'], $_POST['quantity'], $_POST['price'])";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+</body>
+</html>
